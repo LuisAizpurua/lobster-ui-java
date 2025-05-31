58f8bc6cb1ca992eb8227b5bd7b545d5c7e50829
Luis Aizpurua <moisesluis0403@gmail.com>
2025-05-31 05:29:35 -0500

    Update class.java

--- src/com/ds3/proyecto1/Presentation.java
+++ src/com/ds3/proyecto1/Presentation.java
@@ -6,8 +6,8 @@ public class Presentation implements IteratorFields{
     private final String university = "Universidad Tecnologica de Panama";
     private final String faculty = "Facultad De Sistema Computacional";
     private final String career = "Desarrollo Y Gestion De Software";
-    private final String subject = "Desarrollo De Software III";
-    private final String teacher = "Ricardo Chan";
+    private final String subject = "Desarrollo De Software";
+    private final String teacher = "******";
 
     @Override
     public List<String> listFields() {
